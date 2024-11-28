a31831d53b90bdeb7236542be248ca08c582f998
¨Jane <smoreira11@hotmail.com>
2024-11-28 22:23:38 +0900

    add javascript

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function bucleFor(num) {
-  for(let i=0; i<num; i++){
-    console.log(i);
-  }
-}
-bucleFor(11)
+const saludar = function(nombre) {
+  return "hola" + nombre;
+};
 
-
-Math.PI
-Math.E
-Math.random()
-Math.pow(2,8)
-Math.min(2,4,6)
-
+saludar("carlos")
