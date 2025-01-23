c16a3a5300976b35e67676378532546a397e4896
¨Jane <smoreira11@hotmail.com>
2025-01-23 22:21:00 +0900

    add more

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Contador Simple</h1>
+      <div className="toolbar"  >
+      <textarea  id="editor" ></textarea>
+        </div>
+      <hr></hr>
+      <div  className="toolbar"   >
+      <p id="preview"></p>
+       </div>
       <p>Has hecho clic {count} veces</p>
       <button onClick={() => setCount(count + 1)}>Incrementar</button>
     </div>
